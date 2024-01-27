8adafbd3361adcaf2635d834fedf369add56c6d7
Jimmy Ostler <jtostler1@gmail.com>
2024-01-26 21:47:21 -0800

    basic interrupts handles

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,19 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(jimos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
-mod serial;
+pub mod vga_buffer;
+pub mod serial;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
+use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Got it :{}:", "sunglasses");
+
+    jimos::init();
 
     #[cfg(test)]
     test_main();
@@ -33,7 +21,6 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-use core::panic::PanicInfo;
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -44,28 +31,10 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-
-    loop {}
-}
-
-
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-
-    exit_qemu(QemuExitCode::Success);
+    jimos::test_panic_handler(info)
 }
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
-    assert_ne!(1, 1);
-    serial_println!("[ok]");
+    assert_eq!(1, 1);
 }
